86e5ef85ec36e36880737eb667fecc62fc66c027
nicolash92 <nicolas.hanout@gmail.com>
2019-05-15 20:09:48 -0700

    Added config files and api routes

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const dbURI = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true
+    });
+
+    console.log(`DB Connected`);
+  } catch (err) {
+    console.error(err.message);
+
+    //fail
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
